169da8a18aca1a1844b906e80c17ecdad5a4c692
JustPlay-Code <64895799+JustPlay-Code@users.noreply.github.com>
2022-02-03 11:26:30 +0100

    LINK WORK

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require("express")
+const path = require('path')
 const ip = require("ip")
 
-const { json } = require("express/lib/response")
+// const { json } = require("express/lib/response")
 const app = express()
 
 // "localhost"
 const IPv4 = ip.address()
 const port = 25565
-const IP = IPv4
+const IP = "localhost"
+
+// app.use(express.static(path.join(__dirname, './App/Css')))
+// app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile('Home.html', {
+        root: path.join(__dirname, './App/')
+    })
+})
 
 app.listen(port, IP, () => {
     console.log(`Chat aperta su ${IP}:${port}`)
+    console.log(path.join(__dirname, './App/'))
 })
 
-app.use(express.static("public"))
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+app.get("/login", (req, res) => {
+    res.sendFile('Login.html', {
+        root: path.join(__dirname, './App/')
+    })
 })
