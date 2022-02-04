577875500865c1bd44c46eddd6d9534ade9715ac
JustPlay-Code <64895799+JustPlay-Code@users.noreply.github.com>
2022-02-04 18:57:49 +0100

    popup start

--- App/JavaScript/Home.js
+++ App/JavaScript/Home.js
@@ -1,3 +1,11 @@
 joinChat = () => {
     window.location.href = "/chat";
 }
+
+togglePopup = () => {
+    const popup = document.getElementsByClassName("displayPopup");
+    console.log("premuto")
+    popup.style.display = "none" ?
+    popup.style.display = "block" :
+    popup.style.display = "none";
+}
