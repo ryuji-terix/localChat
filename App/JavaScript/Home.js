6d2bccc7eae2a53c09eb7f5bdbdc0fe145941e85
JustPlay-Code <64895799+JustPlay-Code@users.noreply.github.com>
2022-02-05 16:12:53 +0100

    finally login screen
    missing: order and fetch

--- App/JavaScript/Home.js
+++ App/JavaScript/Home.js
@@ -1,13 +1,30 @@
-joinChat = () => {
-    window.location.href = "/chat";
-}
+const l_c = document.getElementById("login_container");
+const l_d = document.getElementById("login_div");
+
+l_c.addEventListener("click", () => {
+    togglePopup();
+})
 
 togglePopup = (admin) => {
-    if (admin) {
-        console.log("admin");
+    // if (admin) {
+    //     console.log("admin");
+
+    // } else {
+    //     console.log("povero stolto");
+
+    // }
+    
+    if (l_c.style.display === "none") {
+        l_d.style.display = "flex";
+        l_c.style.display = "block";
 
     } else {
-        console.log("povero stolto");
-
+        l_d.style.display = "none";
+        l_c.style.display = "none";
     }
+
+
 }
+
+
+
