7ddf2b02e67a05ee7a80917d7809f463d9c64264
JustPlay-Code <64895799+JustPlay-Code@users.noreply.github.com>
2022-02-06 18:27:27 +0100

    Login init

--- App/JavaScript/Home.js
+++ App/JavaScript/Home.js
@@ -1,30 +1,72 @@
 const l_c = document.getElementById("login_container");
-const l_d = document.getElementById("login_div");
+const form = document.getElementById("login_form");
+const main = document.getElementById("main");
+const psw_label = document.getElementById("login_password_label");
+const psw_login = document.getElementById("login_password");
+const name_login = document.getElementById("login_name");
+const subimt = document.getElementById("submit");
+const error = document.getElementById("error_login");
+var admin = false;
 
 l_c.addEventListener("click", () => {
     togglePopup();
 })
 
+
 togglePopup = (admin) => {
-    // if (admin) {
-    //     console.log("admin");
+    if (admin) {
+        psw_label.style.display = "block";
+        psw_login.style.display = "block";
+        form.classList.remove("limit");
 
-    // } else {
-    //     console.log("povero stolto");
-
-    // }
-    
-    if (l_c.style.display === "none") {
-        l_d.style.display = "flex";
-        l_c.style.display = "block";
-
+        psw_login.setAttribute("required", "");
+        var admin = true;
     } else {
-        l_d.style.display = "none";
-        l_c.style.display = "none";
+        psw_label.style.display = "none";
+        psw_login.style.display = "none";
+        form.classList.add("limit");
+
+        psw_login.removeAttribute("required");
+        var admin = false;
     }
 
+    if (l_c.style.display === "none") {
+        form.style.display = "flex";
+        l_c.style.display = "block";
+        main.style.display = "none";
 
+    } else {
+        form.style.display = "none";
+        l_c.style.display = "none";
+        main.style.display = "flex";
+    }
 }
 
+form.addEventListener("submit",
+    async function sendCredential(event) {
+        event.preventDefault();
 
+        userN = form.elements["name"].value
+        userP = form.elements["password"].value
 
+        const data = {
+            user: userN,
+            password: userP,
+            time: Date.now(),
+        };
+
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
+
+        const response = await fetch("/login", options);
+        const json = await response.json();
+
+        console.log(json);
+        // await Promise.resolve()
+    }
+)
--- LocalChat.js
+++ LocalChat.js
@@ -1,32 +1,69 @@
 const express = require("express")
+// const bcrypt = require('bcryptjs')
+const Datastore = require("nedb")
 const path = require('path')
 const ip = require("ip")
 
+const blankArray = []
+const equals = (a, b) => {
+    return JSON.stringify(a) === JSON.stringify(b);
+}
+
+
+
 // const { json } = require("express/lib/response")
+const credential = new Datastore("credential.db")
+// const message = new Datastore("message.db")
 const app = express()
+
+
 
 // "localhost"
 const IPv4 = ip.address()
 const port = 25565
 const IP = IPv4
 
+app.listen(port, IP, () => {
+    console.log(`Local Chat open at ${IP}:${port}`)
+})
+
+
+
 // Importing file
 app.use(express.static(__dirname))
-// app.use(express.json());
+credential.loadDatabase()
+// message.loadDatabase()
+app.use(express.json());
 
+
+
+// Page loading
 app.get("/", (req, res) => {
     res.sendFile('Home.html', {
         root: path.join(__dirname, './App/Html')
     })
 })
 
-app.listen(port, IP, () => {
-    console.log(`Local Chat open at ${IP}:${port}`)
-})
+app.post("/login", (req, res) => {
+    credential.find({ user: req.body.user }, (e, doc) => {
+        if (equals(doc, blankArray)) {
+            credential.insert(req.body)
+            console.log(req.body)
 
-app.get("/login", (req, res) => {
-    res.sendFile('Login.html', {
-        root: path.join(__dirname, './App/Html/')
+            res.json({
+                user: req.body.user,
+                password: req.body.password,
+                time: req.body.time
+            })
+            res.status(201) //Object created
+        } else {
+            res.status(400) //Bad Request
+            console.error(`User (${req.body.user}) already exist`)
+
+            res.send(`User "${req.body.user}" already exist`)
+
+            res.status(201)
+        }
     })
 })
 
@@ -35,3 +72,17 @@ app.get("/chat", (req, res) => {
         root: path.join(__dirname, './App/Html/')
     })
 })
+
+
+
+// Request handler
+
+
+
+// req.query.password = bcrypt.hashSync(req.query.password, 10)
+// app.get("/alldb", (req, res) => {
+//     database.find({}, (err, data) => {
+//         err ? res.end() : ""
+//         res.json(data)
+//     })
+// })
