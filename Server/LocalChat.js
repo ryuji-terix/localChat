d52bea063d077d0058a7f96f643a6fc58253dce7
JustPlay-Code <64895799+JustPlay-Code@users.noreply.github.com>
2022-02-16 15:22:52 +0100

    WHY NOTHING WORK AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Server/LocalChat.js
+++ Server/LocalChat.js
@@ -1,54 +1,43 @@
 // * Importing libraries
-const func = require("./Util/Function")
+const fun = require("./Util/function")
 const express = require('express')
 const bcrypt = require('bcryptjs')
-const Datastore = require('nedb')
-const path = require('path')
-const ip = require('ip')
+
 
 // * Variable
-const localChatDir = func.localChatDir()
-require('dotenv').config({ path: path.join(localChatDir, '/Server/Util/.env') })
+const localChatDir = fun.localChatDir()
 
 
 // * Setup thingy
 const { json } = require('express/lib/response')
-const credential = new Datastore({ filename: path.join(localChatDir, '/Server/Database/credential.db') })
-const message = new Datastore({ filename: path.join(localChatDir, '/Server/Database/message.db') })
 const app = express()
 
 
 // * File management
 app.use(express.static(localChatDir))
-credential.loadDatabase()
 app.use(express.json())
-message.loadDatabase()
+fun.initDb()
 
 
 // * Startup
-const IPv4 = ip.address()
-const port = 25565
-const IP = IPv4
-
-app.listen(port, IP, () => {
-    console.log(`Local Chat open at http://${IP}:${port}/`)
+app.listen(fun.IP("port"), fun.IP("ip"), () => {
+    console.log(`Local Chat open at http://${fun.IP()}/`)
 })
 
 
 // * Front page
 app.get('/', (req, res) => {
     console.log(`${req.headers['x-forwarded-for'] || req.connection.remoteAddress} logged`)
-    func.sendFile(res, 'Home.html')
+    fun.sendFile(res, 'Home.html')
     
 })
 
 
-// * Login part
+// * Get user
 app.get('/users', (req, res) => {
-    let doc = func.getUserby(res, {})
-    func.isArrEqual(doc, []) ? res.send("empty") : res.send(doc)
+    console.log(fun.getUserby(res, { }))
+    // fun.isArrEqual(doc, []) ? res.send("empty") : res.send(doc)
 })
-
 
 
 // ? create user
@@ -56,11 +45,11 @@ app.post('/user', async (req, res) => {
     try {
         if (req.body.name === undefined ||
             req.body.password === undefined ||
-            !func.validPassword(req.body.password)) {
-                func.statusExport(res, 400)
+            !fun.validPassword(req.body.password)) {
+                fun.statusExport(res, 400)
                 
-            } else if ( func.isArrEqual(func.getUserby(res, { name: req.body.name }), []) ? false : true ) {
-                func.statusExport(res, 409)
+            } else if ( fun.isArrEqual(fun.getUserby(res, { name: req.body.name }), []) ? false : true ) {
+                fun.statusExport(res, 409)
                 
             } else {
                 let user = {
@@ -68,55 +57,48 @@ app.post('/user', async (req, res) => {
                     password: await bcrypt.hash(req.body.password, 10),
                     time: Date.now()
                 }
-                func.newUser(user)
+                fun.newUser(user)
                 
             }
             
     } catch {
-        func.statusExport(res, 500)
+        fun.statusExport(res, 500)
     }
         
 })
     
     
     // ? login
-    app.post("/user/login", (req, res) => {
-        credential.find({ name: req.body.name }, async (err, doc) => {
-            if (err !== null) {
-                func.statusExport(res, 500)
-                console.error(`Database error "credential": ${err}`)
-                
-            } else {
-                if (func.isArrEqual(doc, [])) {
-                    func.statusExport(res, 400)
-                    
-                } else {
-                    try {
-                        // TODO passing "credential to the page"
-                        await bcrypt.compare(req.body.password, doc[0].password) ? res.redirect("/Chat")
-                        : func.statusExport(res, 401)
-                        
-                    } catch {
-                        func.statusExport(res, 500)
-                    }
-                
-            }
+app.post("/user/login", async (req, res) => {
+    let doc = fun.getUserby(res, { name: req.body.name })
+    if (fun.isArrEqual(doc, [])) {
+        fun.statusExport(res, 400)
+        
+    } else {
+        try {
+            // TODO passing "credential to the page"
+            await bcrypt.compare(req.body.password, doc[0].password)
+            ? res.redirect("/Chat") : fun.statusExport(res, 401)
+            
+        } catch {
+            fun.statusExport(res, 500)
+
         }
-    })
     
+    }   
 })
 
 
 // ? page for the registering
 app.get('/register', (req, res) => {
-    func.sendFile(res, 'Register.html')
+    fun.sendFile(res, 'Register.html')
 })
 
 
 // * Get method
 // ? send chat page
 app.get("/chat", (req, res) => {
-    func.sendFile(res, 'Chat.html')
+    fun.sendFile(res, 'Chat.html')
 })
 
 // // app.get('/chat/:id', (req, res) => {
@@ -152,5 +134,5 @@ app.get("/chat", (req, res) => {
         // // })
         
 app.get('*', (req, res) => {
-    func.statusExport(res, 404)
+    fun.statusExport(res, 404)
 })
--- Server/Util/Function.js
+++ Server/Util/Function.js
@@ -1,12 +1,33 @@
 // * Importing lib
+const Datastore = require('nedb')
 const path = require('path')
+const ip = require('ip')
+
 
 // * Declaring const
 const localChatDir = path.dirname(path.dirname((__dirname)))
+const credential = new Datastore({ filename: path.join(localChatDir, '/Server/Database/credential.db') })
+const message = new Datastore({ filename: path.join(localChatDir, '/Server/Database/message.db') })
+require('dotenv').config({ path: path.join(localChatDir, '/Server/Util/.env') })
 
 
+// ? Random function
 exports.localChatDir = () => {
     return localChatDir
+}
+
+exports.IP = (param) => {
+    if (param === "ip") {
+        return ip.address()
+
+    } else if (param === "port") {
+        return process.env.PORT
+
+    } else {
+        return `${this.IP("ip")}:${this.IP("port")}`
+
+    }
+
 }
 
 exports.validPassword = (psw) => {
@@ -25,6 +46,11 @@ exports.isAdmin = (name) => {
 
 
 // ? Database stuff
+exports.initDb = () => {
+    credential.loadDatabase()
+    message.loadDatabase()
+}
+
 exports.newUser = (user) => {
     credential.insert(user, (err, newDoc) => {
         if (err !== null) {
@@ -39,7 +65,7 @@ exports.newUser = (user) => {
 }
 
 exports.getUserby = (res, param) => {
-    credential.find({ param }, (err, doc) => {
+    credential.find(param, (err, doc) => {
         if (err !== null) {
             func.statusExport(res, 500)
             console.error(`Database error "credential": ${err}`)
